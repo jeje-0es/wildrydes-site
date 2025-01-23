5aacb3a73a363d6c2822b2f00f1bb05d4132620b
jeje-0es <nafisahraja01@gmail.com>
2025-01-23 23:26:34 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_M1WdD5oR9', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'ls4dt7gfjabracl6hlk1umdpf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5pa843t0bhqn9q4m2jq9naner9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
